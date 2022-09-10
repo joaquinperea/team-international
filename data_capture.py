3190d0cee847dc902fdf305c456a1ab556619f78
root <root@pop-os.localdomain>
2022-09-10 17:53:46 -0300

    Add method created

--- data_capture.py
+++ data_capture.py
@@ -8,3 +8,16 @@ class DataCapture:
         """
         self.values_added = []
         self.values_stats = {}
+
+    def add(self, value):
+        """
+        Function that adds new elements to the "values_added" list.
+        :param value: input value to add.
+        """
+        try:
+            if isinstance(value, int) and 0 <= value < 1000:
+                self.values_added.append(value)
+            else:
+                print('Value inserted is not an integer, is not positive or higher than 1000.')
+        except Exception as e:
+            print(f'Exception found on add method: {e}')
