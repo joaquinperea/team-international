92bcee7b8f7be86d51c4a2678193006039c53206
root <root@pop-os.localdomain>
2022-09-10 18:13:48 -0300

    Stats methods added

--- data_capture.py
+++ data_capture.py
@@ -21,3 +21,25 @@ class DataCapture:
                 print('Value inserted is not an integer, is not positive or higher than 1000.')
         except Exception as e:
             print(f'Exception found on add method: {e}')
+
+    def build_stats(self):
+        """
+        Function that generates the necessary statistics structure to be consumed
+        through the "stats" object.
+        :return: Stats object.
+        """
+        try:
+            self.values_added.sort()
+            for i, elem in enumerate(self.values_added):
+                if elem not in self.values_stats:
+                    self.values_stats[elem] = {
+                        'left': i,
+                        'right': len(self.values_added) - i - 1,
+                        'count': 1
+                    }
+                else:
+                    self.values_stats[elem]['right'] = len(self.values_added) - i - 1
+                    self.values_stats[elem]['count'] += 1
+            return Stats(self.values_added, self.values_stats)
+        except Exception as e:
+            print(f'Exception found on build_stats method: {e}')
--- stats.py
+++ stats.py
@@ -7,3 +7,49 @@ class Stats:
         """
         self.values_added = values_added
         self.values_stats = values_stats
+
+    def less(self, value):
+        """
+        Accepts an input value and check all less values added in values list.
+        :param value: value to evaluate.
+        :return: less values.
+        """
+        try:
+            return self.values_stats[value]['left']
+        except Exception as e:
+            if value not in self.values_added:
+                print('Input value was not be added before.')
+            else:
+                print(f'Exception found on less method: {e}')
+
+    def between(self, min_value, max_value):
+        """
+        Accepts two input values and check all between values added in values list.
+        Include minimum and maximun value.
+        :param min_value: minimum value to evaluate.
+        :param max_value: maximum value to evaluate.
+        :return: between values.
+        """
+        try:
+            return len(self.values_added) - \
+                   self.values_stats[min_value]['left'] - \
+                   self.values_stats[max_value]['right']
+        except Exception as e:
+            if min_value or max_value not in self.values_added:
+                print('Some of values were not be added before.')
+            else:
+                print(f'Exception found on between method: {e}')
+
+    def greater(self, value):
+        """
+        Accepts an input value and check all greater values added in values list.
+        :param value: value to evaluate.
+        :return: greater values.
+        """
+        try:
+            return self.values_stats[value]['right']
+        except Exception as e:
+            if value not in self.values_added:
+                print('Input value was not be added before.')
+            else:
+                print(f'Exception found on between method: {e}')
