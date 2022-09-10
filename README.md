5d898cd296182112107b647a2f5a01a5577da838
root <root@pop-os.localdomain>
2022-09-10 18:50:18 -0300

    Tests made

--- README.md
+++ README.md
@@ -1,3 +1,32 @@
+
 # Team International Trial
+
 This is a solution to the evaluation presented 
 by Team International in its hiring process.
+
+Author: Eng. Joaquin Perea
+
+
+
+## SetUp
+
+This project only use "pytest" library for testing porpouses.
+To setup this solution, create a virtual environment and activate it:
+
+```bash
+  python3 -m pip install --user virtualenv
+  python3 -m venv /path/to/new/virtual/environment
+  source venv/bin/activate
+```
+
+After you activate the virtual environment, install "pytest":
+
+```bash
+  python3 -m pip install --user pytest
+```
+## Testing
+To run project tests after installing "pytest", execute in console:
+
+```bash
+  pytest
+```
---
+++ test/test_stats.py
@@ -0,0 +1,65 @@
+import pytest
+from data_capture import DataCapture
+
+
+@pytest.mark.parametrize(
+    "values_added, input_value, expected",
+    [
+        ([3, 9, 3, 4, 6], 4, 2),  # Positive case
+        ([2, 10, 15, 20, 20, 5, 6, 7], 15, 5),  # Positive case.
+        ([2, 10, 15, 20, 20, 'somestring', 6, 7], 15, 4),  # Validating string input.
+        ([2, 10, 15, 20, 20.5, 5, 6, 7], 15, 5),  # Validating float input.
+        ([2, 10, 15, 20, 1000, 5, 6, 7], 15, 5),  # Validating input equal to 1000.
+        ([2, 10, 15, 20, 1020, 5, 6, 7], 15, 5),  # Validating input up than 1000.
+        ([2, 10, 15, 20, -1, 5, 6, 7], 15, 5),  # Validating input less than 0.
+        ([2, 10, 15, 20, 0, 5, 6, 7], 15, 6)  # Validating input equal to 0.
+    ]
+)
+def test_less(values_added, input_value, expected):
+    capture = DataCapture.DataCapture()
+    for i in values_added:
+        capture.add(i)
+    stats = capture.build_stats()
+    assert stats.less(input_value) == expected
+
+
+@pytest.mark.parametrize(
+    "values_added, input_value, expected",
+    [
+        ([3, 9, 3, 4, 6], 4, 2),  # Positive case.
+        ([2, 10, 15, 20, 20, 5, 6, 7], 15, 2),  # Positive case.
+        ([2, 10, 15, 20, 'somestring', 5, 6, 7], 15, 1),  # Validating string input.
+        ([2, 10, 15, 20, 20.5, 5, 6, 7], 15, 1),  # Validating float input.
+        ([2, 10, 15, 20, 1000, 5, 6, 7], 15, 1),  # Validating input equal to 1000.
+        ([2, 10, 15, 20, 1020, 5, 6, 7], 15, 1),  # Validating input up than 1000.
+        ([2, 10, 15, 20, -1, 5, 6, 7], 15, 1),  # Validating input less than 0.
+        ([2, 10, 15, 20, 0, 5, 6, 7], 15, 1)  # Validating input equal to 0.
+    ]
+)
+def test_greater(values_added, input_value, expected):
+    capture = DataCapture.DataCapture()
+    for i in values_added:
+        capture.add(i)
+    stats = capture.build_stats()
+    assert stats.greater(input_value) == expected
+
+
+@pytest.mark.parametrize(
+    "values_added, input_min_value, input_max_value, expected",
+    [
+        ([3, 9, 3, 4, 6], 3, 6, 4),  # Positive case.
+        ([2, 10, 15, 20, 20, 5, 6, 7], 6, 15, 4),  # Positive case.
+        ([2, 10, 15, 20, 20, 5, 6, 'somestring'], 6, 15, 3),  # Validating string input.
+        ([2, 10, 15, 20, 20.5, 5, 6, 7], 6, 15, 4),  # Validating float input.
+        ([2, 10, 15, 20, 1000, 5, 6, 7], 6, 15, 4),  # Validating input equal to 1000.
+        ([2, 10, 15, 20, 1020, 5, 6, 7], 6, 15, 4),  # Validating input up than 1000.
+        ([2, 10, 15, 20, -1, 5, 6, 7], 6, 15, 4),  # Validating input less than 0.
+        ([2, 10, 15, 20, 0, 5, 6, 7], 6, 15, 4)  # Validating input equal to 0.
+    ]
+)
+def test_between(values_added, input_min_value, input_max_value, expected):
+    capture = DataCapture.DataCapture()
+    for i in values_added:
+        capture.add(i)
+    stats = capture.build_stats()
+    assert stats.between(input_min_value, input_max_value) == expected
